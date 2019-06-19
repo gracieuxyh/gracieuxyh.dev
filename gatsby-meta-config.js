d063ca8694ca743977bff3c1b1f002fc763e1e13
박영호 <mellonia.bluerose@gmail.com>
2019-06-19 13:11:15 +0900

    update gatsby-meta-config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  title: `Bee starter`,
+  title: `gracieuxyh's Devlog`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  author: `gracieuxyh`,
+  introduction: `잡식성 프론트엔드 개발자, 무엇에 관심이 있다고 하기엔 실력이 형편없어서 좀 더 본인을 갈고 닦은 후 덧붙일 예정.`,
+  siteUrl: `https://gracieuxyh.github.io`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `https://github.com/gracieuxyh`, // Your GitHub account
     medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
+    facebook: `https://www.facebook.com/gracieuxyh`, // Your Facebook account
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'gracieuxyh/gracieuxyh.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'gracieuxyh',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
