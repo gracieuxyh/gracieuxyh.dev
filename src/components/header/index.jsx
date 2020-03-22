81806467a3269a114bd7be035f28e452e0527cfc
박영호 <mellonia.bluerose@gmail.com>
2020-03-22 10:47:38 +0900

    update style

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import './index.scss'
 
@@ -9,10 +8,7 @@ export const Header = ({ location, rootPath }) => {
     isRoot && (
       <header className="header">
         <h1>POSTS</h1>
-        <p>
-          공부와 개발의 기록 · 각종 리뷰 · <del>아주 가끔</del> 잡담을
-          기록합니다.
-        </p>
+        <p>기술 공유 · 각종 리뷰 · 소소한 일상</p>
       </header>
     )
   )
